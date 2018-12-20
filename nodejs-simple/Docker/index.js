2eae2977d9173b129d5d1db995241a9eb23641bd
vineeth.vijayan@lunatech.nl <vineeth.vijayan@lunatech.nl>
2018-12-20 17:11:34 +0100

    node app update

--- nodejs-simple/Docker/index.js
+++ nodejs-simple/Docker/index.js
@@ -1,13 +1,19 @@
 var express = require('express');
 
 // Constants
+
 var DEFAULT_PORT = 8080;
 var PORT = process.env.PORT || DEFAULT_PORT;
+var counter = 0;
+
+var os = require("os");
+var hostname = os.hostname();
 
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World\n');
+  counter += 1;
+  res.send('Hello World -' + counter + '-' + hostname);
 });
 
 app.listen(PORT)
